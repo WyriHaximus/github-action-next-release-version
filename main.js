b3071d417d5a1a0bde38c1974498fcee45c69287
Cees-Jan Kiewiet <ceesjank@gmail.com>
2023-01-25 23:37:02 +0100

    Switch to filebased outputs

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
+const fs = require('fs');
 const currentVersion = process.env.INPUT_VERSION.match(/\d+/g).map(Number).pop();
 const nextVersion = currentVersion + 1;
 
-console.log(`::set-output name=version::${nextVersion}`);
-console.log(`::set-output name=r_version::r${nextVersion}`);
+fs.appendFileSync(process.env.GITHUB_OUTPUT, `version=${nextVersion}\n`);
+fs.appendFileSync(process.env.GITHUB_OUTPUT, `r_version=r${nextVersion}\n`);
 
 process.exit(0);
